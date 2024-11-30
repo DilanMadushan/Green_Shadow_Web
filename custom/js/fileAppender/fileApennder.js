4be975e2de7cbc570a6d5baef8c862b9d82dd885
Dilan Fernando <dilanmadushanka6013@gmail.com>
2024-12-01 00:03:43 +0530

    Field image upload is done

--- custom/js/fileAppender/fileApennder.js
+++ custom/js/fileAppender/fileApennder.js
@@ -1,66 +1,68 @@
-// (function ($) {
-//     'use strict';
-//     $(function () {
-//         $('#field1_upload').on('click', function () {
-//             var file = $(this).parent().parent().parent().find('#fieldImg1_button');
-//             file.trigger('click');
-//         });
+(function ($) {
+    'use strict';
+    $(function () {
+        $('#field1_upload').on('click', function () {
+            var file = $(this).parent().parent().parent().find('#fieldImg1_button');
+            file.trigger('click');
+        });
 
-//         $('#fieldImg1_button').on('change', function () {
-//             // Update the input field with the selected file's name
-//             $(this).parent().find('#field1_input').val($(this).val().replace(/C:\\fakepath\\/i, ''));
+        $('#fieldImg1_button').on('change', function () {
+            // Update the input field with the selected file's name
+            $(this).parent().find('#field1_input').val($(this).val().replace(/C:\\fakepath\\/i, ''));
 
-//             // Handle image preview
-//             const file = this.files[0];
-//             if (file) {
-//                 const reader = new FileReader();
-//                 reader.onload = function (e) {
-//                     $("#fieldImg1_previw").attr("src", e.target.result);
-//                      const imagefile = e.target.result.toString();
-//                      console.log(imagefile);
+            // Handle image preview
+            const file = this.files[0];
+            if (file) {
+                const reader = new FileReader();
+                reader.onload = function (e) {
+                    $("#fieldImg1_previw").attr("src", e.target.result);
+                    $("#field1_base64_input").val(e.target.result.toString());
+
+                    console.log($("#field1_base64_input").val());
                     
-//                 };
-//                 reader.readAsDataURL(file);
-//             } else {
-//                 // Clear the preview if no file is selected
-//                 $("#fieldImg1_previw").attr("src", "");
-//             }
-//         });
+                };
+                reader.readAsDataURL(file);
+            } else {
+                // Clear the preview if no file is selected
+                $("#fieldImg1_previw").attr("src", "");
+            }
+        });
 
-//     });
-// })(jQuery);
+    });
+})(jQuery);
 
 
-// (function ($) {
-//     'use strict';
-//     $(function () {
-//         $('#field2_upload').on('click', function () {
-//             var file = $(this).parent().parent().parent().find('#fieldImg2_button');
-//             file.trigger('click');
-//         });
+(function ($) {
+    'use strict';
+    $(function () {
+        $('#field2_upload').on('click', function () {
+            var file = $(this).parent().parent().parent().find('#fieldImg2_button');
+            file.trigger('click');
+        });
 
-//         $('#fieldImg2_button').on('change', function () {
-//             // Update the input field with the selected file's name
-//             $(this).parent().find('#field2_input').val($(this).val().replace(/C:\\fakepath\\/i, ''));
+        $('#fieldImg2_button').on('change', function () {
+            // Update the input field with the selected file's name
+            $(this).parent().find('#field2_input').val($(this).val().replace(/C:\\fakepath\\/i, ''));
 
-//             // Handle image preview
-//             const file = this.files[0];
-//             if (file) {
-//                 const reader = new FileReader();
-//                 reader.onload = function (e) {
-//                     // Set the image preview source
-//                     $("#fieldImg2_previw").attr("src", e.target.result);
-//                     imagefile = e.target.result.toString(); 
-//                 };
-//                 reader.readAsDataURL(file);
-//             } else {
-//                 // Clear the preview if no file is selected
-//                 $("#fieldImg2_previw").attr("src", "");
-//             }
-//         });
+            // Handle image preview
+            const file = this.files[0];
+            if (file) {
+                const reader = new FileReader();
+                reader.onload = function (e) {
+                    // Set the image preview source
+                    $("#fieldImg2_previw").attr("src", e.target.result);
+                    $("#field2_base64_input").val(e.target.result.toString());
+                    
+                };
+                reader.readAsDataURL(file);
+            } else {
+                // Clear the preview if no file is selected
+                $("#fieldImg2_previw").attr("src", "");
+            }
+        });
 
-//     });
-// })(jQuery);
+    });
+})(jQuery);
 
 
 // (function ($) {
